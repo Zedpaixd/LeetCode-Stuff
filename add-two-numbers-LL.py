b89eab49173f957f8b1a16694debba4b57373448
Armand Alexandru Balint <70021545+Zedpaixd@users.noreply.github.com>
2022-02-02 14:07:06 +0200

    Update add-two-numbers-LL.py

--- add-two-numbers-LL.py
+++ add-two-numbers-LL.py
@@ -1,4 +1,4 @@
-# https://leetcode.com/problems/add-two-numbers/   Python3   120 ms | 13.9 MB
+# https://leetcode.com/problems/add-two-numbers/   Python3   112 ms | 13.9 MB    - Less memory than 99.34% of python3 submissions
 
 # Definition for singly-linked list.
 # class ListNode:
@@ -16,13 +16,11 @@ class Solution:
         
         while l1 or l2:
             
-            if l1:
-                finsum += (l1.val * tens) 
-                l1 = l1.next
-                
-            if l2:
-                finsum += (l2.val * tens) 
-                l2 = l2.next
+            if l1 or l2:
+                finsum += ((l1.val if l1 else 0) * tens) 
+                l1 = (l1.next if l1 else None)
+                finsum += ((l2.val if l2 else 0) * tens) 
+                l2 = (l2.next if l2 else None)
                 
             tens *= 10
             
@@ -34,5 +32,3 @@ class Solution:
             output = output.next
             
         return finalOutput.next
-        
-        
